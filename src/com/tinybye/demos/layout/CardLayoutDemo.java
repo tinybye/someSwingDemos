8b6689f3197b7831c5fd7c83a89e56a513bab7ba
tinybye <tinybye@gmail.com>
2022-09-20 09:07:06 +0800

    CardLayoutDemo

--- src/com/tinybye/demos/layout/CardLayoutDemo.java
+++ src/com/tinybye/demos/layout/CardLayoutDemo.java
@@ -14,8 +14,8 @@ import java.awt.*;
 public class CardLayoutDemo {
     public static void main(String[] agrs) {
         JFrame frame = new JFrame("卡片布局长啥样");
-        JPanel panel1 = new JPanel();
-        JPanel panel2 = new JPanel();
+        JPanel indexPanel = new JPanel();
+        JPanel loginPanel = new JPanel();
         // 卡片式布局的面板
         CardLayout cardLayout = new CardLayout();
         frame.setLayout(cardLayout);
@@ -23,22 +23,22 @@ public class CardLayoutDemo {
         JButton loginButton = new JButton("去登录");
         loginButton.addActionListener(e -> {
             // 让卡片布局管理器展示名为card2的面板
-            cardLayout.show(frame.getContentPane(), "card2");
+            cardLayout.show(frame.getContentPane(), "loginCard");
         });
-        panel1.add(loginButton);
-        panel1.add(new JButton("去注册"));
-        panel2.add(new JTextField("用户名", 20));
-        panel2.add(new JTextField("密码", 20));
+        indexPanel.add(loginButton);
+        indexPanel.add(new JButton("去注册"));
+        loginPanel.add(new JTextField("用户名", 20));
+        loginPanel.add(new JTextField("密码", 20));
         JButton returnButton = new JButton("返回首页");
         returnButton.addActionListener(e -> {
             // 让卡片布局管理器展示名为card1的面板
-            cardLayout.show(frame.getContentPane(), "card1");
+            cardLayout.show(frame.getContentPane(), "indexCard");
         });
-        panel2.add(returnButton);
-        // 添加面板1，将该面板命名为card1
-        frame.add(panel1, "card1");
-        // 添加面板2，将该面板命名为card2
-        frame.add(panel2, "card2");
+        loginPanel.add(returnButton);
+        // 添加面板1，将该面板命名为indexCard
+        frame.add(indexPanel, "indexCard");
+        // 添加面板2，将该面板命名为loginCard
+        frame.add(loginPanel, "loginCard");
         frame.setBounds(300, 200, 300, 200);
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
